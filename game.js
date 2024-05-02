812e622947b3a7f558da8d83f1d440a96705cc80
dnisaev <dnisaev@yandex.ru>
2024-05-02 18:37:11 +0300

    add sprint5/week2 game solution (beta)

--- game.js
+++ game.js
@@ -1,137 +1,158 @@
 class Game {
-  #settings = {
-    gridSize: {
-      width: 4,
-      height: 5
+    #settings = {
+        gridSize: {
+            width: 4,
+            height: 5
+        },
+        googleJumpInterval: 2000,
+    };
+    #status = "pending";
+    #player1;
+    #player2;
+    #google;
+
+    constructor() {
+
     }
-  };
-  #status = "pending";
-  #player1;
-  #player2;
-  #google;
 
-  constructor() {
+    async start() {
+        if (this.#status === "pending") {
+            this.#status = "in-process";
+        }
+        this.#createUnits()
 
-  }
-
-  set settings(newSetting) {
-    this.#settings = newSetting;
-  }
-
-  get settings() {
-    return this.#settings;
-  }
-
-  get status() {
-    return this.#status
-  }
-
-  get player1() {
-    return this.#player1
-  }
-
-  get player2() {
-    return this.#player2
-  }
-
-  get google() {
-    return this.#google
-  }
-
-  #createUnits() {
-    const maxWidth = this.#settings.gridSize.width;
-    const maxHeight = this.#settings.gridSize.height;
-
-    const player1Position = new Position(
-      Position.getNotCrossedPosition(
-        [],
-        maxWidth,
-        maxHeight ));
-    this.#player1 = new Player(player1Position, 1);
-
-    const player2Position = new Position(
-      Position.getNotCrossedPosition(
-        [player1Position],
-        maxWidth,
-        maxHeight ));
-    this.#player2 = new Player(player2Position, 2);
-
-    const googlePosition = new Position(
-      Position.getNotCrossedPosition(
-        [player1Position, player2Position],
-        maxWidth,
-        maxHeight ));
-    this.#google = new Google(googlePosition)
-
-  }
-
-  start() {
-    if (this.#status === "pending") {
-      this.#status = "in-process";
+        setInterval(() => {
+            const googlePosition = new Position(
+                Position.getNotCrossedPosition([
+                        this.#player1.position,
+                        this.#player2.position,
+                        this.#google.position],
+                    this.#settings.gridSize.width,
+                    this.#settings.gridSize.height)
+            );
+            this.#google = new Google(googlePosition)
+        }, this.#settings.googleJumpInterval)
     }
-    this.#createUnits()
-  }
 
-  pause() {
+    pause() {
 
-  }
+    }
 
-  resume() {
+    resume() {
 
-  }
+    }
 
-  finished() {
+    finished() {
 
-  }
+    }
+
+    #createUnits() {
+        const maxGridWidthSize = this.#settings.gridSize.width;
+        const maxGridHeightSize = this.#settings.gridSize.height;
+
+        const player1Position = new Position(
+            Position.getNotCrossedPosition(
+                [],
+                maxGridWidthSize,
+                maxGridHeightSize));
+        this.#player1 = new Player(player1Position, 1);
+
+        const player2Position = new Position(
+            Position.getNotCrossedPosition(
+                [player1Position],
+                maxGridWidthSize,
+                maxGridHeightSize));
+        this.#player2 = new Player(player2Position, 2);
+
+        const googlePosition = new Position(
+            Position.getNotCrossedPosition(
+                [player1Position, player2Position],
+                maxGridWidthSize,
+                maxGridHeightSize));
+        this.#google = new Google(googlePosition)
+
+    }
+
+    set settings(newSetting) {
+        this.#settings = newSetting;
+    }
+
+    get settings() {
+        return this.#settings;
+    }
+
+    get status() {
+        return this.#status
+    }
+
+    get player1() {
+        return this.#player1
+    }
+
+    get player2() {
+        return this.#player2
+    }
+
+    get google() {
+        return this.#google
+    }
 }
 
 class NumberUtil {
-  static getRandomNumber(max) {
-    return Math.floor(1 + Math.random() * (max));
-  }
+    static getRandomNumber(max) {
+        return Math.floor(1 + Math.random() * (max));
+    }
 }
 
 class Unit {
-  constructor(position) {
-    this.position = position;
-  }
+    constructor(position) {
+        this.position = position;
+    }
 }
 
 class Player extends Unit {
-  constructor(position, playerNumber) {
-    super(position)
-    this.playerNumber = playerNumber;
-  }
+    constructor(position, playerNumber) {
+        super(position)
+        this.playerNumber = playerNumber;
+    }
 }
 
 class Google extends Unit {
-  constructor(position) {
-    super(position)
-  }
+    constructor(position) {
+        super(position)
+    }
 }
 
 class Position {
-  constructor(obj) {
-    this.x = obj.x;
-    this.y = obj.y;
-  }
+    constructor(obj) {
+        this.x = obj.x;
+        this.y = obj.y;
+    }
 
-  static getNotCrossedPosition(coordinates, maxX, maxY) {
-    let x, y;
+    static getNotCrossedPosition(coordinates, maxX, maxY) {
+        let x, y;
 
-    do {
-      x = NumberUtil.getRandomNumber(maxX);
-      y = NumberUtil.getRandomNumber(maxY);
-    } while (
-      coordinates.some(
-        coord => coord.x === x && coord.y === y
-      )
-      );
+        do {
+            x = NumberUtil.getRandomNumber(maxX);
+            y = NumberUtil.getRandomNumber(maxY);
+        } while (
+            coordinates.some(
+                coord => coord.x === x && coord.y === y
+            )
+            );
 
-    return { x, y };
-  }
+        return {x, y};
+    }
+
+    clone() {
+        return new Position({x: this.x, y: this.y})
+    }
+
+    equal(otherPosition) {
+        return otherPosition.x === this.x && otherPosition.y === this.y
+    }
 }
 
 module.exports = {
-  Game
+    Game
 };
--- game.test.js
+++ game.test.js
@@ -1,65 +1,88 @@
-const { Game } = require("./game.js");
+const {Game} = require("./game.js");
 
 describe("game test", () => {
-  it("init test", () => {
-    const game = new Game();
+    it("init test", () => {
+        const game = new Game();
 
-    game.settings = {
-      gridSize: {
-        width: 4,
-        height: 5
-      }
-    };
+        game.settings = {
+            gridSize: {
+                width: 4,
+                height: 5
+            }
+        };
 
-    expect(game.settings.gridSize.width).toBe(4);
-    expect(game.settings.gridSize.height).toBe(5);
-  });
+        expect(game.settings.gridSize.width).toBe(4);
+        expect(game.settings.gridSize.height).toBe(5);
+    });
 
-  it("start game", () => {
-    const game = new Game();
+    it("start game", () => {
+        const game = new Game();
 
-    game.settings = {
-      gridSize: {
-        width: 4,
-        height: 5
-      }
-    };
+        game.settings = {
+            gridSize: {
+                width: 4,
+                height: 5
+            }
+        };
 
-    expect(game.status).toBe('pending');
-    game.start();
-    expect(game.status).toBe('in-process');
-  });
+        expect(game.status).toBe('pending');
+        game.start();
+        expect(game.status).toBe('in-process');
+    });
 
-  it("units should have unique coordinates", () => {
-    const game = new Game();
+    it("units should have unique coordinates", () => {
+        const game = new Game();
 
-    game.settings = {
-      gridSize: {
-        width: 1,
-        height: 3
-      }
-    };
+        game.settings = {
+            gridSize: {
+                width: 1,
+                height: 3
+            }
+        };
 
-    game.start();
+        game.start();
 
-    console.log(game.player1);
-    console.log(game.player2);
-    console.log(game.google);
+        console.log(game.player1);
+        console.log(game.player2);
+        console.log(game.google);
 
-    expect([1]).toContain(game.player1.position.x)
-    expect([1, 2, 3]).toContain(game.player1.position.y)
+        expect([1]).toContain(game.player1.position.x)
+        expect([1, 2, 3]).toContain(game.player1.position.y)
 
-    expect([1]).toContain(game.player2.position.x)
-    expect([1, 2, 3]).toContain(game.player2.position.y)
+        expect([1]).toContain(game.player2.position.x)
+        expect([1, 2, 3]).toContain(game.player2.position.y)
 
-    expect([1]).toContain(game.google.position.x)
-    expect([1, 2, 3]).toContain(game.google.position.y)
+        expect([1]).toContain(game.google.position.x)
+        expect([1, 2, 3]).toContain(game.google.position.y)
 
-    expect(
-      game.google.position.x !== game.player1.position.x ||
-      game.google.position.y !== game.player2.position.y ||
-      (game.google.position.y !== game.player1.position.y ||
-        game.google.position.x !== game.player2.position.x)
-    )
-  })
+        expect(
+            game.google.position.x !== game.player1.position.x ||
+            game.google.position.y !== game.player2.position.y ||
+            (game.google.position.y !== game.player1.position.y ||
+                game.google.position.x !== game.player2.position.x)
+        )
+    });
+
+    it("check google position after jump", async () => {
+
+        const game = new Game();
+
+        game.settings = {
+            gridSize: {
+                width: 1,
+                height: 4
+            },
+            googleJumpInterval: 100,
+        };
+
+        await game.start();
+
+        const prevPosition = game.google.position.clone();
+        await delay(150);
+        expect(game.google.position.equal(prevPosition)).toBe(false)
+    })
 });
+
+const delay = (ms) => new Promise((res) => {
+    setTimeout(res, ms)
+})
